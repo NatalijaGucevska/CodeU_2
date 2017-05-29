de6417d5abb7ff2039587e2e68253bdd280a549a
NatalijaGucevska <natalija.gucevska@epfl.ch>
2017-05-29 14:23:29 +0200

    Print ancestors of a node

--- BinaryTree.java
+++ BinaryTree.java
@@ -27,4 +27,44 @@ public class BinaryTree<T> {
 		}
 		return false;
 	}
+	
+	public void printAncestors(T val) {
+		ArrayList<Node<T>> ancestors = findAncestors(val); 
+		for(Node<T> a: ancestors) {
+			System.out.print(a.getVal() + " ");
+		}
+	}
+
+	private ArrayList<Node<T>> findAncestors(T val) {
+		Node<T> node = findNode(root.getLeft(), root.getRight(), val);
+		ArrayList<Node<T>> ancestors = new ArrayList<>();
+		while (node != null && node.getParent() != null) {
+			node = node.getParent();
+			ancestors.add(node);
+		}
+		return ancestors;
+	}
+
+	private Node<T> findNode(Node<T> left, Node<T> right, T val) {
+		if (left != null) {
+			if (left.getVal().equals(val)) {
+				return left;
+			} else {
+				Node<T> l = findNode(left.getLeft(), left.getRight(), val);
+				if (l != null)
+					return l;
+			}
+		}
+		if (right != null) {
+			if (right.getVal().equals(val)) {
+				return right;
+			} else {
+				Node<T> r = findNode(right.getLeft(), right.getRight(), val);
+				if (r != null)
+					return r;
+			}
+		}
+		return null;
+	}
+
 }
